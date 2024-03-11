2ff3eafd72affd90a22808272717909e6dde6fc9
cksajil <sajildcb@gmail.com>
2024-03-11 16:09:02 +0530

    updated sketch

--- arduino_codes/kws_custom/kws_custom.ino
+++ arduino_codes/kws_custom/kws_custom.ino
@@ -3,7 +3,8 @@
 #include <PDM.h>
 #include "model.h"
 #include <TensorFlowLite.h>
-#include "tensorflow/lite/micro/micro_error_reporter.h"
+#include "tensorflow/lite/micro/all_ops_resolver.h"
+#include "tensorflow/lite/micro/tflite_bridge/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/micro_mutable_op_resolver.h"
 #include "tensorflow/lite/schema/schema_generated.h"
@@ -61,7 +62,7 @@ void setup()
 
     static tflite::MicroErrorReporter micro_error_reporter;
     error_reporter = &micro_error_reporter;
-
+    tflite::ops::micro::AllOpsResolver resolver;
     PDM.onReceive(onPDMdata);
     PDM.setGain(40);
 
@@ -75,18 +76,18 @@ void setup()
     arm_rfft_init_q15(&S_q15, WINDOW_SIZE, 0, 1);
 
     const tflite::Model *model = ::tflite::GetModel(model);
-    if (model->version() != TFLITE_SCHEMA_VERSION)
-    {
-        TF_LITE_REPORT_ERROR(error_reporter,
-                             "Model provided is schema version %d not equal "
-                             "to supported version %d.",
-                             model->version(), TFLITE_SCHEMA_VERSION);
-        return;
-    }
-    static tflite::MicroMutableOpResolver<5> micro_op_resolver(error_reporter);
+    // if (model->version() != TFLITE_SCHEMA_VERSION)
+    // {
+    //     TF_LITE_REPORT_ERROR(error_reporter,
+    //                          "Model provided is schema version %d not equal "
+    //                          "to supported version %d.",
+    //                          model->version(), TFLITE_SCHEMA_VERSION);
+    //     return;
+    // }
+
     // Build an interpreter to run the model with.
     static tflite::MicroInterpreter static_interpreter(
-        model, micro_op_resolver, tensor_arena, kTensorArenaSize, error_reporter);
+        model, resolver, tensor_arena, kTensorArenaSize, error_reporter);
     interpreter = &static_interpreter;
     // Allocate memory from the tensor_arena for the model's tensors.
     TfLiteStatus allocate_status = interpreter->AllocateTensors();
