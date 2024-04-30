436baab23929ff432bef22ef99677122eb95d8a1
cksajil <sajilck@gmail.com>
2024-04-30 12:45:01 +0530

    Added MFCC computing sample

--- Tutorials/01_Compute_MFCC/compute_mfcc/compute_mfcc.ino
+++ Tutorials/01_Compute_MFCC/compute_mfcc/compute_mfcc.ino
@@ -1,42 +1,75 @@
 #include <PDM.h>
+#include "mfcc.h"
 
+#define SAMPLES 160
 #define SAMPLING_FREQUENCY 16000
-#define NUM_SAMPLES 512
-short sampleBuffer[NUM_SAMPLES];
+#define NUM_FRAMES 100
+#define NUM_MFCC_COEFFS 13
+#define MFCC_DEC_BITS 4
+#define FRAME_SHIFT_MS 30
+#define FRAME_SHIFT ((int16_t)(SAMPLING_FREQUENCY * 0.001 * FRAME_SHIFT_MS))
+#define MFCC_BUFFER_SIZE (NUM_FRAMES * NUM_MFCC_COEFFS)
+#define FRAME_LEN_MS 30
+#define FRAME_LEN ((int16_t)(SAMPLING_FREQUENCY * 0.001 * FRAME_LEN_MS))
+
+short audio_buffer[SAMPLES];
+int recording_win = 100;
 volatile int samplesRead;
 
+int num_frames = NUM_FRAMES;
+int num_mfcc_features = NUM_MFCC_COEFFS;
+int frame_shift = FRAME_SHIFT;
+int frame_len = FRAME_LEN;
+int mfcc_dec_bits = MFCC_DEC_BITS;
+
+MFCC *mfcc;
+float *mfcc_buffer;
+
 void onPDMdata(void);
+void extract_features(int16_t *audio_buffer, float *mfcc_buffer);
 
 void setup()
 {
     Serial.begin(9600);
     while (!Serial)
     {
-    };
+    }
 
     PDM.onReceive(onPDMdata);
-    PDM.setBufferSize(NUM_SAMPLES);
+    PDM.setBufferSize(NUM_FRAMES);
     PDM.setGain(40);
     if (!PDM.begin(1, SAMPLING_FREQUENCY))
     {
         Serial.println("Failed to start PDM!");
     }
+
+    mfcc = new MFCC(num_mfcc_features, frame_len, mfcc_dec_bits);
+    mfcc_buffer = new float[NUM_FRAMES * NUM_MFCC_COEFFS];
 }
 
 void loop()
 {
     if (samplesRead)
     {
-        for (int i = 0; i < NUM_SAMPLES; i++)
+        extract_features(audio_buffer, mfcc_buffer);
+
+        for (int i = 0; i < MFCC_BUFFER_SIZE; i++)
         {
-            Serial.println(sampleBuffer[i]);
+            Serial.println(mfcc_buffer[i]);
         }
+
         samplesRead = 0;
     }
 }
 
+void extract_features(int16_t *audio_buffer, float *mfcc_buffer)
+{
+    int32_t mfcc_buffer_head = (num_frames - recording_win) * num_mfcc_features;
+    mfcc->mfcc_compute(audio_buffer, &mfcc_buffer[mfcc_buffer_head]);
+}
+
 void onPDMdata()
 {
-    int bytesRead = PDM.read((uint8_t *)sampleBuffer, NUM_SAMPLES * sizeof(short));
+    int bytesRead = PDM.read((uint8_t *)audio_buffer, NUM_FRAMES * sizeof(short));
     samplesRead = bytesRead / sizeof(short);
 }
